81654ebdd1143fe0e96fa6b732481f656267dd6b
DanielDinshtein <danieldinsh@gmail.com>
2022-02-09 20:46:16 +0200

    Add Header.js Start working on - WelcomeView

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import "./WordChoosingView.css";
+
+const Header = (props) => {
+    return (
+        <div className="App-header">
+            <h1>{props.headerTitle}</h1>
+        </div>
+    );
+};
+
+export default Header;
--- src/views/WelcomeView.js
+++ src/views/WelcomeView.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import "./WelcomeView.css";
 
 const WelcomeView = (props) => {
+    const userName = useRef();
+
+    // TODO: Remember props.onStartGame func
+    const startGameHandler = (event) => {
+        event.preventDefault();
+        console.log(userName.current.value);
+    };
+
     return (
         <div className="welcome-view">
-            <h2>Welcome View</h2>
+            <form onSubmit={startGameHandler}>
+                <div className="user-name">
+                    <h4>Please Enter Your User Name</h4>
+                    <input type="text" className="user-input" placeholder="user name" ref={userName} required />
+                </div>
+                <button type="submit" className="submit-btn">
+                    Start Game
+                </button>
+            </form>
         </div>
     );
 };
