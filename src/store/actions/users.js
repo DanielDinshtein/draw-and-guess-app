15a58759df834e4b526d7aaf37af6885f68180b4
DanielDinshtein <danieldinsh@gmail.com>
2022-02-11 23:26:31 +0200

    users - Login

--- src/store/actions/users.js
+++ src/store/actions/users.js
@@ -1,42 +1,39 @@
 export const LOGIN = "LOGIN";
 
-// const serverUrl = "";
 
 export const login = (username) => {
-    let firstPlayer = "no";
-    if (username === "daniel") {
-        firstPlayer = "yes";
-    }
-    sessionStorage.setItem("firstPlayer", JSON.stringify(firstPlayer));
-    return { type: LOGIN, username: username, firstPlayer: firstPlayer };
+    return async (dispatch) => {
+
+        const requestUrl = process.env.REACT_APP_SERVER_URL + "/players/login";
+
+        try {
+            const response = await fetch(requestUrl, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    username: username,
+                }),
+            });
+
+            const data = await response.json();
+
+            if (response.status === 200) {
+
+                const firstPlayer = data.newGame ? "yes" : "no";
+                sessionStorage.setItem("firstPlayer", JSON.stringify(firstPlayer));
+                dispatch({ type: LOGIN, username: username, firstPlayer: firstPlayer });
+
+            } else if (response.status === 401) {
+
+                let message = data.message;
+                throw new Error(message);
+            }
+
+        } catch (err) {
+            // TODO: Check The Reason To The Error
+            let message = "Error in auth/authenticate";
+            console.log(message);
+            throw new Error(message);
+        }
+    };
 };
-
-// TODO: Good One - Server
-// export const authenticate = (username) => {
-//     return async (dispatch) => {
-//         const requestUrl = serverUrl + "/auth";
-//         try {
-//             const response = await fetch(requestUrl, {
-//                 method: "POST",
-//                 headers: {
-//                     "Content-Type": "application/json",
-//                 },
-//                 body: JSON.stringify({
-//                     username: username,
-//                 }),
-//             });
-//             // TODO: Response.json() -> data -> firstPlayer
-//             if (response.status !== 200) {
-//                 let message = "This username is already taken, please choose another one";
-//                 throw new Error(message);
-//             }
-
-//             sessionStorage.setItem("authenticated", JSON.stringify("yes"));
-//             dispatch({ type: AUTHENTICATE, username: username, firstPlayer });
-//         } catch (err) {
-//             // TODO: Check The Reason To The Error
-//             let message = "Error in auth/authenticate";
-//             throw new Error(message);
-//         }
-//     };
-// };
--- src/views/WelcomeView.js
+++ src/views/WelcomeView.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-import * as authActions from "../store/actions/users";
+import * as usersActions from "../store/actions/users";
 
 import "./WelcomeView.css";
 
@@ -16,7 +16,7 @@ const WelcomeView = (props) => {
         event.preventDefault();
 
         try {
-            await dispatch(authActions.login(username.current.value));
+            await dispatch(usersActions.login(username.current.value));
             const firstPlayer = JSON.parse(sessionStorage.getItem("firstPlayer"));
 
             if (firstPlayer) {
