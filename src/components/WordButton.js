03ba69895a6fa43260540d155c3cb4bab1a16df6
DanielDinshtein <danieldinsh@gmail.com>
2022-02-11 02:35:10 +0200

    WordChoosing - Before Add Custom Button

---
+++ src/components/WordButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import '../views/WordChoosingView.css';
+
+const WordButton = (props) => {
+    return (
+        <div className="word-container">
+            <button className="btn" id={props.difficulty} onClick={props.wordChoose}>
+                {props.word}
+            </button>
+            <div className="word-difficulty">
+                <p className="difficulty">{props.difficulty} - </p>
+                <p className="points">{props.points} pt</p>
+            </div>
+        </div>
+    );
+};
+
+export default WordButton;
--- src/views/WordChoosingView.js
+++ src/views/WordChoosingView.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState, useCallback, useRef } from "react";
 
 import { getWords } from "../utils/wordsVocabulary";
 
+import WordButton from "../components/WordButton";
+
 import "./WordChoosingView.css";
 
+const words = getWords();
+
 const WordChoosingView = (props) => {
-    const words = getWords();
+    const [word, setWord] = useState("");
+    const startDisabled = useRef(true);
+
+    const wordChoose = useCallback((event) => {
+        setWord(words[event.target.id]);
+        startDisabled.current = false;
+    }, []);
+
+    const onStartDrawingHandler = () => {
+        console.log(startDisabled.current);
+    };
 
     return (
         <div className="word-choosing-view">
             <h3>Choose Word :</h3>
             <div className="choose-word">
+                <WordButton word={words.easy} difficulty={"easy"} wordChoose={wordChoose} points={1} />
                 <div className="word-container">
-                    <button
-                        className="btn"
-                        id="easy"
-                        onClick={() => {
-                            console.log("easy");
-                        }}
-                    >
+                    <button className="btn" id="easy" onClick={wordChoose}>
                         {words.easy}
                     </button>
                     <div className="word-difficulty">
@@ -27,13 +36,7 @@ const WordChoosingView = (props) => {
                     </div>
                 </div>
                 <div className="word-container">
-                    <button
-                        className="btn"
-                        id="medium"
-                        onClick={() => {
-                            console.log("medium");
-                        }}
-                    >
+                    <button className="btn" id="medium" onClick={wordChoose}>
                         {words.medium}
                     </button>
                     <div className="word-difficulty">
@@ -42,13 +45,7 @@ const WordChoosingView = (props) => {
                     </div>
                 </div>
                 <div className="word-container">
-                    <button
-                        className="btn"
-                        id="hard"
-                        onClick={() => {
-                            console.log("hard");
-                        }}
-                    >
+                    <button className="btn" id="hard" onClick={wordChoose}>
                         {words.hard}
                     </button>
                     <div className="word-difficulty">
@@ -57,12 +54,11 @@ const WordChoosingView = (props) => {
                     </div>
                 </div>
             </div>
-            <button
-                className="start-drawing-btn"
-                onClick={() => {
-                    console.log("Start Drawing");
-                }}
-            >
+            <div className="show-word">
+                <p id="word-chose">The word you chose</p>
+                <p>&nbsp;-&nbsp;{word}</p>
+            </div>
+            <button className="start-drawing-btn" disabled={startDisabled.current} onClick={onStartDrawingHandler}>
                 Start Drawing
             </button>
         </div>
