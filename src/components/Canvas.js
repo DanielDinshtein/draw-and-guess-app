15d72ee036954897bfb7da5974e75cb4e6909567
DanielDinshtein <danieldinsh@gmail.com>
2022-02-14 00:11:26 +0200

    Finish Canvas Component

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,50 +1,18 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { ReactSketchCanvas } from "react-sketch-canvas";
 
-import CanvasOptions from "./CanvasOptions";
 import "../views/DrawingView.css";
 
-const styles = {
-	border: "0.0625rem solid #9c9c9c",
-	borderRadius: "0.25rem",
-};
-
-const initialProps = {
-	strokeColor: "#000000",
-	canvasColor: "#FFFFFF",
-	strokeWidth: 4,
-	eraserWidth: 8,
-};
-
 const Canvas = (props) => {
-	let canvas = useRef();
 
-	const [strokeColorState, setStrokeColorState] = useState("#000000");
-	const [canvasColorState, setCanvasColorState] = useState("#FFFFFF");
-	const [strokeWidthState, setStrokeWidthState] = useState(4);
-	const [eraserWidthState, setEraserWidthState] = useState(8);
-
-	const canvasState = {
-		strokeColor: { value: strokeColorState, setter: setStrokeColorState },
-		canvasColor: { value: canvasColorState, setter: setCanvasColorState },
-		strokeWidth: { value: strokeWidthState, setter: setStrokeWidthState },
-		eraserWidth: { value: eraserWidthState, setter: setEraserWidthState },
-	};
+	const { canvasRef } = props;
 
 	return (
 		<div id="canvas-view">
 			<div id="react-sketch">
-				<ReactSketchCanvas
-					ref={canvas}
-					style={styles}
-					strokeColor={strokeColorState}
-					canvasColor={canvasColorState}
-					strokeWidth={strokeWidthState}
-					eraserWidth={eraserWidthState}
-					{...props}
-				/>
+				<ReactSketchCanvas ref={canvasRef} {...props} />
 			</div>
-			<CanvasOptions canvasRef={canvas} propsState={canvasState} initialPropsState={initialProps} />
+			<div>{props.children}</div>
 		</div>
 	);
 };
--- src/views/DrawingView.js
+++ src/views/DrawingView.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 import Canvas from "../components/Canvas";
+import CanvasOptions from "../components/CanvasOptions";
 import SubmitButton from "../components/SubmitButton";
 
 import "./DrawingView.css";
 
-const DrawingView = (props) => {
-	let word = useSelector((state) => state.game.word);
+const styles = {
+	border: "0.0625rem solid #9c9c9c",
+	borderRadius: "0.25rem",
+};
 
-	if (!word) {
-		// DELETE: 
-		word = "wordToDraw";
-	}
+const initialProps = {
+	strokeColor: "#000000",
+	canvasColor: "#FFFFFF",
+	strokeWidth: 4,
+	eraserWidth: 8,
+};
+
+const DrawingView = (props) => {
+	const canvas = useRef();
+
+	const [strokeColorState, setStrokeColorState] = useState("#000000");
+	const [canvasColorState, setCanvasColorState] = useState("#FFFFFF");
+	const [strokeWidthState, setStrokeWidthState] = useState(4);
+	const [eraserWidthState, setEraserWidthState] = useState(8);
+
+	const propsStates = {
+		strokeColor: { value: strokeColorState, setter: setStrokeColorState },
+		canvasColor: { value: canvasColorState, setter: setCanvasColorState },
+		strokeWidth: { value: strokeWidthState, setter: setStrokeWidthState },
+		eraserWidth: { value: eraserWidthState, setter: setEraserWidthState },
+	};
+
+	// , wordPoints, totalPoints
+	const { word } = useSelector((state) => state.game);
+
+	const sendDrawHandler = () => {};
 
 	return (
 		<div className="drawing-view">
@@ -21,9 +46,23 @@ const DrawingView = (props) => {
 				<h2>&nbsp;-&nbsp;{word}</h2>
 			</div>
 			<div id="canvas-container">
-				<Canvas />
+				<Canvas
+					canvasRef={canvas}
+					style={styles}
+					strokeColor={strokeColorState}
+					canvasColor={canvasColorState}
+					strokeWidth={strokeWidthState}
+					eraserWidth={eraserWidthState}
+				>
+					<div>
+						<CanvasOptions canvasRef={canvas} propsState={propsStates} initialPropsState={initialProps} />
+
+						<SubmitButton id="send-draw-button" onClick={sendDrawHandler}>
+							Send
+						</SubmitButton>
+					</div>
+				</Canvas>
 			</div>
-			<SubmitButton id="send-draw-button">Send</SubmitButton>
 		</div>
 	);
 };
